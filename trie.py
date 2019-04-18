e2385007fcdeb5fe4f3a7d63122cc44c8d229a5c
Alexandr Orlov <alex.orlov7@gmail.com>
2019-04-18 21:24:32 +0300

    Add lookup with interactive mode

--- trie.py
+++ trie.py
@@ -27,10 +27,21 @@ TRIE = rec_dd()
 ITEMSKEY = "_items"
 
 
+@profile
+def index(items: Iterable[Tuple[int, str]]) -> Set[int]:
+    """Add items to the index and return set of present ids"""
+    for item in tqdm(items):  # * progressbar eats memory, but helps a lot
+        add(*item)
+
+    # * collect returns duplicated ids, but we may use `set` to return unique ids
+    # * and present them in preferred language
+    added = set(collect(TRIE))  # same as the input ids at first call
+    return added
+
+
 def add(id_: int, word: str) -> None:
     """Split word into characters and add to the tree.
-       Insert id into `items` list after last character.
-    """
+       Insert id into `items` list after last character."""
     level = TRIE
     for c in word.lower():  # ? more preprocessing?
         level = level[c]
@@ -43,26 +54,32 @@ def add(id_: int, word: str) -> None:
 
 
 def collect(level: dict) -> Iterable[int]:
-    """Collect items on specified tree level
-    """
+    """Collect items on specified tree level"""
     keys = set(level.keys()) - {ITEMSKEY}
     return chain(level.get(ITEMSKEY, []), *(collect(level[key]) for key in keys))
 
 
+def lookup(level: dict, query: str) -> Iterable[int]:
+    for c in query.lower():
+        level = level.get(c)
+        if not level:
+            return set()
+    return set(collect(level))
+
+
 def analyze(level, info=defaultdict(int)):
-    """Collect tree info - number of levels, key-nodes, item ids
-    """
-    info["levels"] += 1
+    """Collect tree info - key-nodes & item ids"""
     for key in level.keys():
-        info["nodes"] += 1
         if key == ITEMSKEY:
             info["ids"] += len(level[key])
         else:
+            info["nodes"] += 1
             analyze(level[key], info)
     return info
 
 
 def show(level, prefix=""):
+    """Simple tree output"""
     print(prefix)
     for key in level.keys():
         if key == ITEMSKEY:
@@ -72,53 +89,42 @@ def show(level, prefix=""):
 
 
 @profile
-def index(items: Iterable[Tuple[int, str]]) -> Set[int]:
-    """Add items to index and return set of present ids"""
-    for item in tqdm(items):  # * progressbar eats memory, but helps a lot
-        add(*item)
-
-    # * collect returns duplicated ids, but we may use `set` to return unique ids
-    # * and present them in preferred language
-    added = set(collect(TRIE))  # same as input ids
-    return added
-
-
-@profile
-def _words():
+def _make_items():
+    """Test method to create some items"""
 
     words_ = [
-        ("Запорожская область", "Запорізька область"),
-        ("Киевская область", "Київська область"),
-        ("Хмельницкая область", "Хмельницька область"),
-        ("Закарпатская область", "Закарпатська область"),
-        ("Ровенская область", "Рівненська область"),
-        ("Черниговская область", "Чернігівська область"),
-        ("Кировоградская область", "Кіровоградська область"),
-        ("Херсонская область", "Херсонська область"),
-        ("Черкасская область", "Черкаська область"),
-        ("Житомирская область", "Житомирська область"),
-        ("Одесская область", "Одеська область"),
-        ("Львовская область", "Львівська область"),
-        ("Харьковская область", "Харківська область"),
-        ("Волынская область", "Волинська область"),
-        ("Полтавская область", "Полтавська область"),
-        ("Николаевская область", "Миколаївська область"),
-        ("Тернопольская область", "Тернопільська область"),
-        ("Днепропетровская область", "Дніпропетровська область"),
-        ("Крым", "Крим"),
-        ("Черновицкая область", "Чернівецька область"),
-        ("Луганская область", "Луганська область"),
-        ("Донецкая область", "Донецька область"),
-        ("Сумская область", "Сумська область"),
-        ("Ивано-Франковская область", "Івано-Франківська область"),
         ("Винницкая область", "Вінницька область"),
+        ("Волынская область", "Волинська область"),
+        ("Днепропетровская область", "Дніпропетровська область"),
+        ("Донецкая область", "Донецька область"),
+        ("Житомирская область", "Житомирська область"),
+        ("Закарпатская область", "Закарпатська область"),
+        ("Запорожская область", "Запорізька область"),
+        ("Ивано-Франковская область", "Івано-Франківська область"),
+        ("Киевская область", "Київська область"),
+        ("Кировоградская область", "Кіровоградська область"),
+        ("Крым", "Крим"),
+        ("Луганская область", "Луганська область"),
+        ("Львовская область", "Львівська область"),
+        ("Николаевская область", "Миколаївська область"),
+        ("Одесская область", "Одеська область"),
+        ("Полтавская область", "Полтавська область"),
+        ("Ровенская область", "Рівненська область"),
+        ("Сумская область", "Сумська область"),
+        ("Тернопольская область", "Тернопільська область"),
+        ("Харьковская область", "Харківська область"),
+        ("Херсонская область", "Херсонська область"),
+        ("Хмельницкая область", "Хмельницька область"),
+        ("Черкасская область", "Черкаська область"),
+        ("Черниговская область", "Чернігівська область"),
+        ("Черновицкая область", "Чернівецька область"),
     ]
 
-    words_ += [(a[::-1], b[::-1]) for a, b in words_]  # add more fake words
+    words_ += [(a[::-1], b[::-1]) for a, b in words_]  # add even more fake words
 
     alphabet = "".join(sorted(set(c for pair in words_ for c in "".join(pair).lower())))
 
-    id_mul = 1000  # * same words, more ids
+    id_mul = 1  # * same words, more ids
     word_mul = 20  # * more words, same ids
 
     word_len = 2 * len(words_)
@@ -141,15 +147,15 @@ Total words: {id_len * word_mul}
 if __name__ == "__main__":
     import timing  # noqa
 
-    words = _words()
-    added = index(words)
+    items = dict(_make_items())
+    input_ = list(items.items())
+    added = index(input_)
 
-    assert added == set(i for i, _ in words)
+    assert added == set(i for i, _ in input_)
 
-    len_added, len_input = len(added), len(words)
+    len_added, len_input = len(added), len(input_)
 
     info = analyze(TRIE)
-    # show(TRIE)
 
     # `total_size` eats memory too
     print(
@@ -161,3 +167,13 @@ Index Size: {total_size(added)}
 Info: {dict(info)}
     """
     )
+
+    q = 1
+    print("Enter query:")
+    while q:
+        q = input("> ")
+        res = q and lookup(TRIE, q)
+        print([items[r] for r in res])
+
+
+__all__ = ["TRIE", "add", "collect", "index", "lookup"]
