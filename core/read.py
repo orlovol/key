d756d6f1b8203250ebae0277bf06f63241abb20a
Alexandr Orlov <alex.orlov7@gmail.com>
2019-04-27 14:45:49 +0300

    Handle escape correctly in csv reader

--- core/read.py
+++ core/read.py
@@ -9,9 +9,16 @@ class Row(typing.NamedTuple):
     name_uk: str
 
 
+def row_maker(row):
+    try:
+        return Row._make(row)
+    except TypeError as e:
+        raise ValueError(f"Invalid row: {row}") from e
+
+
 def read_csv(path):
     with open(path, newline="") as data:
         data.readline()  # skip header
-        georeader = csv.reader(data)
-        for row in map(Row._make, georeader):
+        georeader = csv.reader(data, escapechar="\\")
+        for row in map(row_maker, georeader):
             yield row
