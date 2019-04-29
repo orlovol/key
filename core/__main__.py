b4d07e91b78db35449c6d4aad18a803179055244
Alexandr Orlov <alex.orlov7@gmail.com>
2019-04-29 17:02:14 +0300

    Add argparse

--- core/__main__.py
+++ core/__main__.py
@@ -1,24 +1,52 @@
 import sys
-import pathlib
+import argparse
 
-from . import utils, engine
+from . import engine, utils
 
-GEOFILE = pathlib.Path(__file__).parent / "data/geo.csv"
-ALLGEOFILE = pathlib.Path(__file__).parent / "data/geo_all.csv"
+
+class DefaultHelpParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write("error: %s\n" % message)
+        self.print_help()
+        sys.exit(2)
 
 
 @utils.profile
 def main():
-    interactive = len(sys.argv) > 1
-    if not interactive:
+
+    parser = DefaultHelpParser(description="Key Search Engine.")
+    parser.add_argument("infile", help="input .csv file with geodata")
+    parser.add_argument(
+        "-e",
+        "--export",
+        choices=["tree", "csv"],
+        help="export data into output file in specified format",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="exported.csv",
+        help="output file where exported data is saved (default: exported.csv)",
+    )
+    parser.add_argument(
+        "-i", "--interactive", action="store_true", help="run in interactive query mode"
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="output detailed info")
+
+    args = parser.parse_args()
+
+    if not args.interactive:
         from . import timing  # noqa
 
-    engie = engine.Engine(file=GEOFILE)
-    engie.info()
-    if interactive:
+    engie = engine.Engine(file=args.infile)
+    if args.verbose:
+        engie.info()
+
+    if args.export:
+        engie.export(args.output, as_tree=args.export == "tree")
+
+    if args.interactive:
         engie.interactive()
-    else:
-        engie.export(ALLGEOFILE)
 
 
 main()
