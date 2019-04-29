e6420a33b5fd1dba94249adf7562912f11822d4d
Alexandr Orlov <alex.orlov7@gmail.com>
2019-04-29 20:08:18 +0300

    Move import/export to data

--- core/data.py
+++ core/data.py
@@ -1,17 +1,19 @@
 import csv
-import typing
-from itertools import chain
 from functools import partial
+from itertools import chain
+from typing import Iterator, NamedTuple, Optional
+
+from . import geo
 
 
-class Row(typing.NamedTuple):
+class Row(NamedTuple):
     geo_id: int
     geo_type: str
     name: str
     name_uk: str
 
 
-class TreeRow(typing.NamedTuple):
+class TreeRow(NamedTuple):
     geo_id: int
     geo_parent_id: int
     geo_type: str
@@ -19,8 +21,20 @@ class TreeRow(typing.NamedTuple):
     name_uk: str
 
 
-def convert_id(i: str) -> typing.Optional[int]:
+# ID helpers
+
+
+def convert_id(i: str) -> Optional[int]:
+    """Convert str id into integer/None"""
     return int(i) if i else None
+
+
+def offset_id(offset: int, id_: int) -> int:
+    """Offset negative integer id by offset number"""
+    return id_ if id_ > 0 else offset - id_
+
+
+# CSV I/O
 
 
 def _row_maker(cls, row):
@@ -51,3 +65,87 @@ def write_csv(path, data):
         )
         for row in data:
             writer.writerow(row)
+
+
+# IMPORT
+
+
+def _read_rows(geo_id: int, geo_type: str, *names: str) -> geo.GeoRecord:
+    """Read denormalized csv rows into GeoRecords.
+    Data should be sorted by area decreasing"""
+    cls = geo.GeoMeta.registry[geo_type]
+    record = geo.GeoRecord(geo_id, cls.from_row_record(*names))
+    return record
+
+
+def _read_tree(geo_id: int, geo_parent_id: int, geo_type: str, *names: str) -> geo.GeoRecord:
+    """Read tree with parent_id csv rows into GeoRecords.
+    Data should be sorted by area decreasing"""
+    cls = geo.GeoMeta.registry[geo_type]
+    parent = geo.GeoRecord.registry[geo_parent_id] if geo_parent_id else None
+    item = cls.from_tree_record(*names, parent=parent)
+    record = geo.GeoRecord(geo_id, item)
+    return record
+
+
+def read_items(csv: str) -> Iterator[geo.GeoRecord]:
+    rows = read_csv(csv)
+    csv_type = next(rows)
+    make_record = _read_tree if csv_type == TreeRow else _read_rows
+    for row in rows:
+        yield make_record(*row)
+
+
+# EXPORT
+
+
+def _collect_names(geo_item: geo.GeoItem) -> Iterator[Iterator[str]]:
+    """Return lang_name tuples for each level, increasing area size"""
+    yield map(str, geo_item)
+    while geo_item.parent:
+        geo_item = geo_item.parent.item
+        yield map(str, geo_item)
+
+
+def collect_names(geo_item: geo.GeoItem) -> Iterator[str]:
+    """Return full names for all languages, decreasing area size"""
+    langs = zip(*_collect_names(geo_item))
+    return (", ".join(lang[::-1]) for lang in langs)
+
+
+def _collect_rows(index: dict) -> Iterator[list]:
+    """Gather data for csv export as denormalized tree"""
+    yield ["geo_id", "geo_type", "name", "name_uk"]
+    # count up to nearest hundred frrom max id, and append added items from there
+    offset = partial(offset_id, (max(index) // 100 + 1) * 100)
+    # sorted by id
+    for key in sorted(index, key=offset):
+        record = index[key]
+        geo_id = offset(record.id)
+        geo_item = record.item
+        geo_type = geo_item.type
+        yield [geo_id, geo_type, *collect_names(geo_item)]
+
+
+def _collect_tree(index: dict) -> Iterator[list]:
+    """Gather data for csv export as tree with parent_id"""
+    yield ["geo_id", "geo_parent_id", "geo_type", "name", "name_uk"]
+    # count up to nearest hundred frrom max id, and append added items from there
+    offset = partial(offset_id, (max(index) // 100 + 1) * 100)
+    order = tuple(geo.GeoMeta.registry)
+    # sorted by decreasing area
+    for key in sorted(index, key=lambda x: order.index(index[x].item.type)):
+        record = index[key]
+        geo_id = offset(record.id)
+        geo_item = record.item
+        geo_parent_id = geo_item.parent and offset(geo_item.parent.id)  # can be None
+        geo_type = geo_item.type
+        yield [geo_id, geo_parent_id, geo_type, *iter(geo_item)]
+
+
+def write_items(data, path, as_tree):
+    collect = _collect_tree if as_tree else _collect_rows
+    write_csv(path, collect(data))
+
+
+__all__ = ["read_items", "write_items"]
--- core/engine.py
+++ core/engine.py
@@ -1,7 +1,6 @@
 from tqdm import tqdm
-from typing import Any, Iterable, Dict, Set, Iterator, List
+from typing import Any, Iterable, Dict, Set, List
 from difflib import SequenceMatcher
-from functools import partial
 
 from . import data, geo, trie, utils
 
@@ -14,54 +13,6 @@ keymap_uk = str.maketrans(
     r"qwertyuiop[]\asdfghjkl;'zxcvbnm,./", r"йцукенгшщзхїґфівапролджєячсмитьбю."
 )
 KEYMAPS = (keymap_uk, keymap_ru)  # ? language preference can be specified somewhere
-
-
-def _collect_names(geo_item: geo.GeoItem) -> Iterator[Iterator[str]]:
-    """Return lang_name tuples for each level, increasing area size"""
-    yield map(str, geo_item)
-    while geo_item.parent:
-        geo_item = geo_item.parent.item
-        yield map(str, geo_item)
-
-
-def collect_names(geo_item: geo.GeoItem) -> Iterator[str]:
-    """Return full names for all languages, decreasing area size"""
-    langs = zip(*_collect_names(geo_item))
-    return (", ".join(lang[::-1]) for lang in langs)
-
-
-def offset_id(offset: int, id_: int) -> int:
-    return id_ if id_ > 0 else offset - id_
-
-
-def collect_rows(index: dict) -> Iterator[list]:
-    """Gather data for csv export as denormalized tree"""
-    yield ["geo_id", "geo_type", "name", "name_uk"]
-    # count up to nearest hundred frrom max id, and append added items from there
-    offset = partial(offset_id, (max(index) // 100 + 1) * 100)
-    # sorted by id
-    for key in sorted(index, key=offset):
-        record = index[key]
-        geo_id = offset(record.id)
-        geo_item = record.item
-        geo_type = geo_item.type
-        yield [geo_id, geo_type, *collect_names(geo_item)]
-
-
-def collect_tree(index: dict) -> Iterator[list]:
-    """Gather data for csv export as tree with parent_id"""
-    yield ["geo_id", "geo_parent_id", "geo_type", "name", "name_uk"]
-    # count up to nearest hundred frrom max id, and append added items from there
-    offset = partial(offset_id, (max(index) // 100 + 1) * 100)
-    order = tuple(geo.GeoMeta.registry)
-    # sorted by decreasing area
-    for key in sorted(index, key=lambda x: order.index(index[x].item.type)):
-        record = index[key]
-        geo_id = offset(record.id)
-        geo_item = record.item
-        geo_parent_id = geo_item.parent and offset(geo_item.parent.id)  # can be None
-        geo_type = geo_item.type
-        yield [geo_id, geo_parent_id, geo_type, *iter(geo_item)]
 
 
 def same_parents(child1: geo.GeoItem, child2: geo.GeoItem) -> bool:
@@ -92,32 +43,6 @@ def same_parents(child1: geo.GeoItem, child2: geo.GeoItem) -> bool:
     return False
 
 
-def read_rows(geo_id: int, geo_type: str, *names: str) -> geo.GeoRecord:
-    """Read denormalized csv rows into GeoRecords.
-    Data should be sorted by area decreasing"""
-    cls = geo.GeoMeta.registry[geo_type]
-    record = geo.GeoRecord(geo_id, cls.from_row_record(*names))
-    return record
-
-
-def read_tree(geo_id: int, geo_parent_id: int, geo_type: str, *names: str) -> geo.GeoRecord:
-    """Read tree with parent_id csv rows into GeoRecords.
-    Data should be sorted by area decreasing"""
-    cls = geo.GeoMeta.registry[geo_type]
-    parent = geo.GeoRecord.registry[geo_parent_id] if geo_parent_id else None
-    item = cls.from_tree_record(*names, parent=parent)
-    record = geo.GeoRecord(geo_id, item)
-    return record
-
-
-def read_items(csv: str) -> Iterator[geo.GeoRecord]:
-    rows = data.read_csv(csv)
-    csv_type = next(rows)
-    make_record = read_tree if csv_type == data.TreeRow else read_rows
-    for row in rows:
-        yield make_record(*row)
-
-
 class Engine:
     def __init__(self, file=None):
         self._trie = trie.Trie()
@@ -127,7 +52,7 @@ class Engine:
         self.lookup = self._trie.lookup
 
         if file:
-            self.index(read_items(file))
+            self.index(data.read_items(file))
 
     @utils.profile
     def index(self, items: Iterable[geo.GeoRecord]) -> None:
@@ -138,9 +63,8 @@ class Engine:
     @utils.profile
     def export(self, path, as_tree=False):
         """Save data from index into csv"""
-        collect = collect_tree if as_tree else collect_rows
-        data.write_csv(path, collect(self._index))
-        print(f"Exported to {path}")
+        data.write_items(self._index, path, as_tree)
+        print(f"Exported {['denormalized', 'tree'][as_tree]} data to {path}")
 
     def search(self, name: str, ids: Set[int]) -> List[geo.GeoRecord]:
         """Find id by exact name in subset of ids"""
