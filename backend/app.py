a9019d508b3795cfb9d62b006042df29415dfedc
Alexandr Orlov <alex.orlov7@gmail.com>
2019-05-01 17:27:42 +0300

    Handle CORS

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask
+from flask_cors import CORS
 
 from .api import api
 from .search_engine import search_engine
 
 app = Flask(__name__.split(".")[0], static_folder=None)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.register_blueprint(api)
 search_engine.init_app(app)
