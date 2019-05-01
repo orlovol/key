66353783a0881631d6b0962d44679c6eca4bbb77
Alexandr Orlov <alex.orlov7@gmail.com>
2019-05-01 13:09:23 +0300

    Lift state up from Search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
+import { debounce } from 'lodash';
+
 import './App.css';
+
+import api from './api'
 import Search from './components/Search'
+import Locations from './components/Locations'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleFetch = this.handleFetch.bind(this)
+  }
+
+  state = {
+    query: 'default text',
+    locations: []
+  }
+
+  handleSearch(locations) {
+    this.setState({ locations: locations })
+  }
+
+  handleFetch = debounce(
+    async function (q) {
+      const res = await fetch(api.location())
+      const data = await res.json()
+      this.setState({ locations: data.results })
+    }
+    , 300
+  )
+
+  handleSubmit(e) {
+    // TODO: implement
+    e.preventDefault()
+  }
+
+  handleChange(e) {
+    this.setState(
+      { query: e.target.value },
+      () => this.handleFetch(this.state.query)
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <Search />
+        <Search
+          query={this.state.query}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <Locations data={this.state.locations} />
       </div>
     );
   }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,43 +1,11 @@
-import React, { Component } from 'react';
-import { debounce } from 'lodash';
+import React from 'react';
 
-class Search extends Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleFetch = this.handleFetch.bind(this)
-  }
-  state = {
-    query: 'default text'
-  }
-  handleSubmit(e) {
-    e.preventDefault()
-  }
-
-  sendRequest = debounce(
-    function (r) {
-      // send request to load suggestions
-      console.log("Send request: " + r)
-    }
-    , 300
-  )
-
-  handleFetch(query) {
-    // fetch suggestions
-    this.sendRequest(query)
-  }
-  handleChange(e) {
-    this.setState({ query: e.target.value }, this.handleFetch(this.state.query))
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
-        <input
-          defaultValue={this.state.query}
-          placeholder="Start typing..." />
-      </form>
-    );
-  }
-}
+const Search = ({ query, handleSubmit, handleChange }) => (
+  <form onSubmit={handleSubmit} onChange={handleChange}>
+    <input
+      defaultValue={query}
+      placeholder="Start typing..." />
+  </form>
+);
 
 export default Search;
